872a9c714133d01753b466695330c3160f2b28db
Tobias Strenger <tobiasrstrenger@gmail.com>
2023-03-13 16:48:05 +0100

    Update main.sql

--- main.sql
+++ main.sql
@@ -9,25 +9,25 @@ SET search_path TO pg_catalog,public,main,spotify,apple;
 CREATE TABLE main.artist (
 	id integer NOT NULL GENERATED ALWAYS AS IDENTITY ,
 	name text,
-	"spoID" char(22),
-	"aplID" integer,
-	"amzID" text,
+	"spotify_id" char(22),
+	"apple_id" integer,
+	"amazon_id" text,
 	CONSTRAINT artist_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE main.album (
 	id integer NOT NULL GENERATED ALWAYS AS IDENTITY ,
-	"spoID" char(22),
-	"aplID" integer,
-	"amzID" text,
+	"spotify_id" char(22),
+	"apple_id" integer,
+	"amazon_id" text,
 	upc char(12),
 	ean char(13),
-	"isSingle" boolean,
-	"isCompilation" boolean,
+	"is_single" boolean,
+	"is_compilation" boolean,
 	name text,
 	label text,
-	"totalTracks" smallint,
-	"releaseDate" date,
+	"total_tracks" smallint,
+	"release_date" date,
 	CONSTRAINT album_pk PRIMARY KEY (id)
 );
 
@@ -35,7 +35,21 @@ CREATE TABLE main.track (
 	id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ,
 	isrc char(12),
 	name text,
-	"trackNumber" smallint,
-	"discNumber" smallint,
+	"track_number" smallint,
+	"disc_number" smallint,
 	CONSTRAINT track_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE main.track_album (
+	track_id bigint NOT NULL,
+	album_id integer NOT NULL,
+	CONSTRAINT track_fk FOREIGN KEY (track_id),
+	CONSTRAINT album_fk FOREIGN KEY (album_id)
+);
+
+CREATE TABLE main.track_artist (
+	track_id bigint NOT NULL,
+	artist_id integer NOT NULL,
+	CONSTRAINT track_fk FOREIGN KEY (track_id),
+	CONSTRAINT artist_fk FOREIGN KEY (artist_id)
 );
